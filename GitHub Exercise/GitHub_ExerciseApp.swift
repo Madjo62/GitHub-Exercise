907bacf75e8f6174a8fefe83b712c5ee4e2c66f1
Ian Gallaugher <ian.gallaugher@gmail.com>
2023-01-28 15:00:25 -0500

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Ian Gallaugher on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
